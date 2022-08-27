be32821bb9cdee50277fb60744423cf2c4c377cd
Grace Akpan <opps4lrng@gmail.com>
2022-08-27 01:17:30 -0600

    reduced number of js files to two: main and vendors

--- js/main.js
+++ js/main.js
@@ -34,16 +34,16 @@ $(document).ready(function () {
   $(".hero").parallax("50%", 0.3);
 
   //Nice scroll initialization
-  $("html").niceScroll({
-    scrollspeed: 50,
-    autohidemode: false,
-    cursorwidth: 8,
-    cursorborderradius: 8,
-    cursorborder: "0",
-    background: "rgba(48, 48, 48, .4)",
-    cursorcolor: "#1f1f1f",
-    zindex: 999,
-  });
+  // $("html").niceScroll({
+  //   scrollspeed: 50,
+  //   autohidemode: false,
+  //   cursorwidth: 8,
+  //   cursorborderradius: 8,
+  //   cursorborder: "0",
+  //   background: "rgba(48, 48, 48, .4)",
+  //   cursorcolor: "#1f1f1f",
+  //   zindex: 999,
+  // });
 
   //Testimonials slider initialization
   $("#tslider").owlCarousel({
